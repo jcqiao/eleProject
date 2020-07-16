7b75e86c52ac242267ad85b4ae3f42fb08e9c3ff
jcq <jiangchun_qiao@163.com>
2020-07-16 14:33:24 +0800

    initialise project

---
+++ src/views/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="index">
+    首页
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
+
+<style scoped>
+  /* .index{
+    width: 100%;
+    height: 100%;
+  } */
+</style>
