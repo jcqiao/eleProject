f5c1116a4e7991e4fc94bb703da48c8d7da28f57
jcq <jiangchun_qiao@163.com>
2020-07-22 16:27:01 +0800

    insert Tabbar COM and data info

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,15 +1,33 @@
 <template>
-  <div class="index">index page
+  <div class="index">
+    <tabbar :tabbarItem="tabbarItem"></tabbar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Tabbar from "components/common/tabbar/Tabbar";
+
 export default {
-  name: 'Index'
-}
+  name: "Index",
+  components: {
+    Tabbar
+  },
+  data() {
+    return {
+      tabbarItem: [
+        { title: "首页", icon: "home", path: "/home" },
+        { title: "订单", icon: "file-text-o", path: "/order" },
+        { title: "我的", icon: "user", path: "/profile" }
+      ]
+    };
+  }
+};
 </script>
 
 <style scoped>
- 
+.index {
+  width: 100%;
+  height: calc(100% - 44px);
+}
 </style>
