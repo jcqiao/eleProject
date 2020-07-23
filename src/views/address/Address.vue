39a14bc251557ffe331b8ec9b708e84e684835f8
jcq <jiangchun_qiao@163.com>
2020-07-23 15:52:56 +0800

    create Address COM

---
+++ src/views/address/Address.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="address">
+    <nav-bar></nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from "components/common/navbar/NavBar";
+export default {
+  name: "Address",
+  components: {
+    NavBar
+  }
+};
+</script>
+
+<style scoped>
+</style>
