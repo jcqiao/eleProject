b5be8bf05dc93a82afaed86f3e9f4dfd3989c525
jcq <jiangchun_qiao@163.com>
2020-07-23 12:50:56 +0800

    App COM: header css

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -26,4 +26,40 @@ export default {
 </script>
 
 <style scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  overflow: auto;
+}
+.header {
+  background: #009eef;
+  color: #fff;
+  font-size: 14px;
+  padding: 16px;
+}
+.address_map {
+  font-weight: 700;
+  margin-bottom: 5px;
+}
+.address_map i {
+  font-size: 18px;
+  margin: 0 5px;
+}
+.address_map span {
+  display: inline-block;
+  width: 85%;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+.shop_search {
+  padding: 5px;
+  background: #fff;
+  color: #ddd;
+  text-align: center;
+  font-size: 12px;
+  border-radius: 4px;
+  padding: 10px 0;
+}
 </style>
