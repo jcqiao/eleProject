e98dc288aa7c433b77ad1dd2fa352500b5a2d8f7
jcq <jiangchun_qiao@163.com>
2020-07-23 11:40:42 +0800

    vuex create state and getters

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,26 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const types = {
+  SET_LOCATION,
+  SET_ADDRESS
+}
+
+const state = {
+  location: {},
+  address: ""
+}
+
+const getters = {
+  location: state => state.location,
+  address: state => state.address
+}
+
+
+
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state,
+  getters,
+  mutations,
+  actions
 })
