80f096506d3ac0f1d67a6f08bd673250c1dfe689
jcq <jiangchun_qiao@163.com>
2020-07-23 12:19:50 +0800

    Home COM: show location with compputed

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="home">
-    首页
+    <div class="header">
+      <div class="address_map">
+        <i class="fa fa-map-marker"></i>
+        <span>{{getLocation}}</span>
+        <i class="fa fa-sort-desc"></i>
+      </div>
+      <div class="shop_search">
+        <i class="fa fa-search"></i>
+        <span>搜索商家</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
-}
+  name: "Home",
+  computed: {
+    getLocation() {
+      return this.$store.getters.location;
+    }
+  }
+};
 </script>
 
 <style scoped>
- 
 </style>
