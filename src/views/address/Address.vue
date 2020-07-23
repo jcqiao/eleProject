8b5eb6a44a397d998e5f25db6ce7d28364c7e17f
jcq <jiangchun_qiao@163.com>
2020-07-23 21:48:02 +0800

    get router params and pass it to SearchCity COM

--- src/views/address/Address.vue
+++ src/views/address/Address.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="address">
     <nav-bar></nav-bar>
+    <search-city :city="city"></search-city>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
+import SearchCity from "./children/SearchCity";
 export default {
   name: "Address",
   components: {
-    NavBar
+    NavBar,
+    SearchCity
+  },
+  data() {
+    return {
+      city: ""
+    };
+  },
+  beforeRouteEnter(to, from, next) {
+    console.log(to);
+    next(vm => {
+      vm.city = to.params.city;
+    });
   }
 };
 </script>
