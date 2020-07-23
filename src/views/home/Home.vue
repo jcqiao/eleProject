33c5f6a5c700096b01fc3b16c7da57a1c0e26031
jcq <jiangchun_qiao@163.com>
2020-07-23 16:31:29 +0800

    modify computed getLocation

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -19,7 +19,7 @@ export default {
   name: "Home",
   computed: {
     getLocation() {
-      return this.$store.getters.location;
+      return this.$store.getters.address;
     }
   },
   methods: {
