0bb77b0f80e205796aee5e324c17150f2e5ea532
jcq <jiangchun_qiao@163.com>
2020-07-23 15:54:03 +0800

    create NavBar COM for Address COM

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="navbar">
+    <nav-item>
+      <div slot="left">
+        <i class="fa fa-chevron-left"></i>
+        <button class="btn">返回</button>
+      </div>
+      <div slot="center">选择收获地址</div>
+    </nav-item>
+  </div>
+</template>
+
+<script>
+import NavItem from "./NavItem";
+export default {
+  name: "Navbar",
+  components: {
+    NavItem
+  }
+};
+</script>
+
+<style scoped>
+.navbar {
+  width: 100%;
+  padding: 5px 10px;
+  align-items: center;
+  background: #009eef;
+  color: #fff;
+}
+.btn {
+  background-color: transparent;
+  border: 0;
+  outline: none;
+  color: #fff;
+}
+</style>
---
+++ src/components/common/navbar/NavItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="nav-item">
+    <div class="nav left">
+      <slot name="left"></slot>
+    </div>
+    <div class="nav center">
+      <slot name="center"></slot>
+    </div>
+    <div class="nav right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavItem"
+};
+</script>
+
+<style scoped>
+.nav-item {
+  display: flex;
+  align-items: center;
+}
+.nav {
+  flex: 1;
+}
+.nav-item .left {
+}
+</style>
