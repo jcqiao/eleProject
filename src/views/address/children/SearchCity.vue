83efb1514a13f9d8ddda9394bc3aaf85a92e0d46
jcq <jiangchun_qiao@163.com>
2020-07-24 09:44:34 +0800

    modify the layout

--- src/views/address/children/SearchCity.vue
+++ src/views/address/children/SearchCity.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="search">
-    <div class="city">
+    <span class="city sea-item">
       {{city}}
       <i class="fa fa-angle-down"></i>
+    </span>
+    <div class="input sea-item">
+      <i class="fa fa-search"></i>
+      <input type="text" v-model="value" placeholder="住宅/公司" />
     </div>
-    <i class="fa fa-search"></i>
-    <input type="text" v-model="value" placeholder="住宅/公司" />
   </div>
 </template>
 
@@ -24,4 +26,31 @@ export default {
 </script>
 
 <style scoped>
+.search {
+  width: 100%;
+  padding: 5px 10px;
+  display: flex;
+  box-sizing: border-box;
+  background: #fff;
+  /* justify-content: space-around; */
+}
+.sea-item {
+  background: #eee;
+  /* border-radius: 5px; */
+}
+.city {
+  flex-grow: 1;
+  /* width: 50px; */
+}
+.input {
+  flex-grow: 3;
+  /* width: 80%; */
+}
+.input input {
+  width: 90%;
+  border: none;
+  outline: none;
+  background: inherit;
+  /* text-align: center; */
+}
 </style>
