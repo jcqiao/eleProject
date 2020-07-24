0c6d1db48c849f07c0c2e6082fb6eeb276bc2b3e
jcq <jiangchun_qiao@163.com>
2020-07-24 11:36:32 +0800

    create location component

---
+++ src/views/address/children/Location.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="location">
+    <div class="title">当前定位</div>
+    <div class="address">
+      <i class="fa fa-location-arrow"></i>
+      {{address}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Location",
+  props: {
+    address: ""
+  }
+};
+</script>
+
+<style scoped>
+.location {
+  padding: 5px 10px;
+  background: #fff;
+}
+.title {
+  padding: 5px;
+  font-size: 10px;
+  color: #333;
+}
+.address {
+  padding: 5px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+</style>
