a736d60fde7466fad57ba212e48c89d38861adbb
jcq <jiangchun_qiao@163.com>
2020-07-24 23:32:02 +0800

    show hotCities

---
+++ src/views/address/children/cityChildren/CityItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="city-item">
+    <div class="hot-wrap">
+      <div class="hot-cities" v-for="(item, index) in hotCities" :key="index">{{item.name}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CityItem",
+  props: {
+    hotCities: [],
+    keys: []
+  }
+};
+</script>
+
+<style scoped>
+</style>
