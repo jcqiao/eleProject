814953533fbafbf207ffe1704998502f78810116
jcq <jiangchun_qiao@163.com>
2020-07-25 14:10:49 +0800

    modify the HTML and show alpha data

--- src/views/address/children/cityChildren/CityItem.vue
+++ src/views/address/children/cityChildren/CityItem.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="city-item">
     <div class="hot-wrap">
-      <div class="hot-cities" v-for="(item, index) in hotCities" :key="index">{{item.name}}</div>
+      <div class="title">热门城市</div>
+      <ul class="hot-cities" v-for="(item, index) in hotCities" :key="index">
+        <li>{{item.name}}</li>
+      </ul>
+    </div>
+    <div class="alpha-item">
+      <div class="keys" v-for="(item, index ) in keys" :key="index">
+        <div>{{item}}</div>
+      </div>
     </div>
   </div>
 </template>
