48d153689765e8f5e647d0baf2224ff16e2077ad
jcq <jiangchun_qiao@163.com>
2020-07-28 14:18:50 +0800

    use Indicator at intercepter to show request/response animation from mint-ui

--- src/main.js
+++ src/main.js
@@ -4,9 +4,33 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import { Indicator } from 'mint-ui'
+
+
+Vue.use(MintUI)
+
 Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
+
+//请求拦截
+axios.interceptors.request.use(config => {
+  Indicator.open()
+  return config
+}, error => {
+  return Promise.reject(error)
+})
+
+//相应拦截
+axios.interceptors.response.use(config => {
+  Indicator.close()
+  return config
+}, error => {
+  Indicator.close()
+  return Promise.reject(error)
+})
 
 new Vue({
   router,
