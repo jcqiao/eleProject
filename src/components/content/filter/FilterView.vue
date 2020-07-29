d2085c825066adda0edd1b18a2a420b06d72b3b8
jcq <jiangchun_qiao@163.com>
2020-07-29 20:21:50 +0800

    create FilterView for home to show filter info

---
+++ src/components/content/filter/FilterView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="filter" v-if="filterData">
+    <aside class="filter_wrap">
+      <div class="filter_item" v-for="(item,index) in filterData.navTab" :key="index">
+        <span>{{item.name}}</span>
+        <i v-if="item.icon" :class="'fa fa-'+item.icon"></i>
+      </div>
+    </aside>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FilterView",
+  props: {
+    filterData: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+.filter {
+  width: 100%;
+  height: 15vw;
+
+  background: #fff;
+}
+.filter_wrap {
+  display: flex;
+  text-align: center;
+  line-height: 15vw;
+  /* justify-content: space-evenly; */
+}
+.filter_item {
+  flex: 1;
+}
+.filter_item i {
+  width: 1.6vw;
+  height: 0.8vw;
+  margin-left: 1.333333vw;
+  margin-bottom: 0.533333vw;
+}
+</style>
